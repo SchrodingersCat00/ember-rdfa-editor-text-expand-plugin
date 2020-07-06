7b38acc3c29a67ddf853c1c96e1e1e577a1fd100
Kobe Mertens <kobe.mertens@hotmail.com>
2020-07-06 17:04:45 +0200

    installed date plugins

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -1,8 +1,14 @@
 export default {
   default: [
+    "rdfa-editor-date-plugin", 
+    "rdfa-editor-date-overwrite-plugin", 
+    "rdfa-editor-dbpedia-info-plugin", 
     "rdfa-editor-wikipedia-slug-plugin"
   ],
   all: [
+    "rdfa-editor-date-plugin",
+    "rdfa-editor-date-overwrite-plugin",
+    "rdfa-editor-dbpedia-info-plugin",
     "rdfa-editor-wikipedia-slug-plugin"
   ],
   none: []
