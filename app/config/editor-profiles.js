c3901c508c3980af2aeb6224b7f49b9d442fde33
Kobe <kobe.mertens@hotmail.com>
2020-07-07 14:05:13 +0200

    commented editor-profiles

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -1,8 +1,8 @@
 export default {
   default: [
-    "rdfa-editor-date-plugin", 
-    "rdfa-editor-date-overwrite-plugin", 
-    "rdfa-editor-dbpedia-info-plugin", 
-    "rdfa-editor-wikipedia-slug-plugin"
+    // "rdfa-editor-date-plugin",
+    // "rdfa-editor-date-overwrite-plugin",
+    // "rdfa-editor-dbpedia-info-plugin",
+    // "rdfa-editor-wikipedia-slug-plugin"
   ]
 };
